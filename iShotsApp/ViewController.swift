196f54c9cc2907a03733112d9374994878611393
wyy1991 <leaves1991@gmail.com>
2015-03-21 19:31:38 -0700

    Initial Commit

---
+++ iShotsApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iShotsApp
+//
+//  Created by yuye wang on 3/21/15.
+//  Copyright (c) 2015 yuye wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
